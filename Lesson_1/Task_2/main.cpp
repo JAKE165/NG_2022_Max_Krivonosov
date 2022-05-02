cfbe6bf52579cf4aaa0ea134e182510a7bf03f7d
JAKE165 <maksimka.krivonosov.2017@gmail.com>
2022-05-02 16:52:09 +0300

    made ASCII art

---
+++ Lesson_1/Task_2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout << R"(             .  . '    .                                            )" << endl
+     << R"(          '   .            . '            .                +            )" << endl
+     << R"(                  `                          '    . '  )" << endl
+     << R"(            .                         ,'`.                         .  )" << endl
+     << R"(       .                  .."    _.-;'    `.              .      )" << endl
+     << R"(                  _.-"`.##%"_.--" ,'        `.           "#"     ___,,od000 )" << endl
+     << R"(               ,'"-_ _.-.--"\   ,'            `-_       '%#%',,/////00000HH )" << endl
+     << R"(             ,'     |_.'     )`/-     __..--""`-_`-._    J L/////00000HHHHM )" << endl
+     << R"(     . +   ,'   _.-"        / /   _-""           `-._`-_/___\///0000HHHHMMM )" << endl
+     << R"(         .'_.-""      '    :_/_.-'                 _,`-/__V__\0000HHHHHMMMM )" << endl
+     << R"(     . _-""                         .        '   _,////\  |  /000HHHHHMMMMM )" << endl
+     << R"(    _-"   .       '  +  .              .        ,//////0\ | /00HHHHHHHMMMMM )" << endl
+     << R"(           `                                   ,//////000\|/00HHHHHHHMMMMMM )" << endl
+     << R"(    .             '       .  ' .   .       '  ,//////00000|00HHHHHHHHMMMMMM )" << endl
+     << R"(         .             .    .    '           ,//////000000|00HHHHHHHMMMMMMM )" << endl
+     << R"(                      .  '      .       .   ,///////000000|0HHHHHHHHMMMMMMM )" << endl
+     << R"(      '             '        .    '         ///////000000000HHHHHHHHMMMMMMM )" << endl
+     << R"(                        +  .  . '    .     ,///////000000000HHHHHHHMMMMMMMM )" << endl
+     << R"(         '      .              '   .       ///////000000000HHHHHHHHMMMMMMMM )" << endl
+     << R"(       '                  . '              ///////000000000HHHHHHHHMMMMMMMM )" << endl
+     << R"(                               .   '      ,///////000000000HHHHHHHHMMMMMMMM )" << endl
+     << R"(           +         .        '   .    .  ////////000000000HHHHHHHHMMMMMMhs )" << endl;
+    return 0;
+}
