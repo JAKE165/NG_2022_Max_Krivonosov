9a0fa0ef59329eacf972dec2c6290443c61097ee
JAKE165 <maksimka.krivonosov.2017@gmail.com>
2022-05-03 19:26:00 +0300

    Task 4 modified

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int CPUprice = 0, Mprice = 0, Vprice = 0, Discount = 0, price = 0;
+    int CPUprice = 0, Mprice = 0, Vprice = 0, Discount = 0;
     cout << "CPU price: ";
     cin >> CPUprice;
     cout << "Motherboard price: ";
@@ -13,7 +13,6 @@ int main()
     cin >> Vprice;
     cout << "Discount: ";
     cin >> Discount;
-    price = (CPUprice + Mprice + Vprice) - ((CPUprice + Mprice + Vprice)*Discount/100);
-    cout << "Your PC will cost - " << price;
+    cout << "Your PC will cost - "  << (CPUprice + Mprice + Vprice) - ((CPUprice + Mprice + Vprice)*Discount/100);
     return 0;
 }
